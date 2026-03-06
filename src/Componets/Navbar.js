0870253ae3032b572a3044e89e11235051345f74
Educaddnta24 <aanianandeducaddnta@gmail.com>
2026-03-06 13:02:03 +0530

    Update Navbar.js

--- src/Componets/Navbar.js
+++ src/Componets/Navbar.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const logoBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAArklEQVR4AWMQ4H8YwOYz///wPIT6D+kgN0CdD/RMtw3ihFCLfD1Jh/Sq4F0B0IoQiGGSc8N3yQ96GEMAMQGsTklKxihgog34RACzUkwkwRgBpDTEsIhpIGDNDG2QDAAKoaDEUMTZCBdA3pIjKJkRABMwyIagI4RJNCNxHyWEDoGgqB+RRCBJRjGlElqBRFgOSph7YioEBA6oIAF7Fq1l/An1UwAAABJRU5ErkJggg==";
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-
   const closeMenu = () => setIsOpen(false);
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <NavLink to="/" className="navbar-logo" onClick={closeMenu}>
-          <img src={"https://www.bmw.in/content/dam/bmw/common/images/logo-icons/BMW/BMW_White_Logo.svg.asset.1670245093434.svg"} alt="BMW Logo" className="navbar-logo-image" />
+          <img
+            src="https://www.bmw.in/content/dam/bmw/common/images/logo-icons/BMW/BMW_White_Logo.svg.asset.1670245093434.svg"
+            alt="BMW Logo"
+            className="navbar-logo-image"
+          />
           BMW
         </NavLink>
 
