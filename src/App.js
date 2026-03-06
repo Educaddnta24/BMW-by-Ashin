85ce40e7d7bdbddf95a885d8c8feb58071a9a0a4
Educaddnta24 <aanianandeducaddnta@gmail.com>
2026-03-06 13:04:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Componets/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import Gallery from './Componets/Gallery';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
+      {/* Display the Gallery component on all pages (or move inside a route if needed) */}
+      <Gallery />
     </div>
   );
 }
